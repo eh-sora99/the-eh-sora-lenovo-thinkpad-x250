d8d5364ed36d5dd43533e07397ab3544825582be
EH-Sora99 <30988679+EH-Sora99@users.noreply.github.com>
2025-11-27 18:51:52 +0100

    hyprland install fixed?

--- Hyprland/HyprInstall.sh
+++ Hyprland/HyprInstall.sh
@@ -16,3 +16,7 @@ sudo dnf install hyprlock -y
 
 sudo dnf install waybar -y
 sudo dnf install rofi -y
+
+dnf install SwayNotificationCenter -y
+dnf install SwayNotificationCenter-git -y
+sudo dnf install wayland-devel wayland-protocols-devel hyprlang-devel pango-devel cairo-devel file-devel libglvnd-devel libglvnd-core-devel libjpeg-turbo-devel libwebp-devel libjxl-devel gcc-c++ hyprutils-devel hyprwayland-scanner
